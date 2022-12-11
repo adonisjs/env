5f0fe9e960cb82e9a0704bd6c952c826f5d20dca
Harminder Virk <virk.officials@gmail.com>
2022-12-11 16:35:49 +0530

    refactor: export EnvProcessor

--- index.ts
+++ index.ts
@@ -10,4 +10,5 @@
 export { Env } from './src/env.js'
 export { EnvParser } from './src/parser.js'
 export { EnvLoader } from './src/loader.js'
+export { EnvProcessor } from './src/processor.js'
 export { InvalidEnvVariablesException } from './src/exceptions/invalid_env_variables.js'
