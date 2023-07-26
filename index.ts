f8c647519bc440b061c03a8856310088f3666165
Harminder Virk <virk.officials@gmail.com>
2023-07-26 10:14:18 +0530

    refactor: export EnvEditor from subpath

--- index.ts
+++ index.ts
@@ -10,6 +10,5 @@
 export { Env } from './src/env.js'
 export { EnvParser } from './src/parser.js'
 export { EnvLoader } from './src/loader.js'
-export { EnvEditor } from './src/editor.js'
 export * as errors from './src/exceptions.js'
 export { EnvProcessor } from './src/processor.js'
