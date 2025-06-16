b1e85fe9b2901b72e5548695eba5ffbc9884aa37
Harminder Virk <virk.officials@gmail.com>
2025-06-16 15:03:05 +0530

    test: fix breaking test

--- tests/validator.spec.ts
+++ tests/validator.spec.ts
@@ -65,7 +65,7 @@ test.group('Env Validator', () => {
       validator.validate({})
     } catch (error) {
       const source = error.stack.split('\n')[2]
-      assert.match(source, new RegExp(import.meta.url))
+      assert.match(source, new RegExp(import.meta.dirname))
     }
   })
 
